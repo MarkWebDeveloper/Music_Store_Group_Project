175440eba80ba82f5f8008a9f47206b5b45f8351
Mark-Kil <markkilcanarias@gmail.com>
2023-10-30 19:28:45 +0100

    menu button black background fixed

--- public/js/banner-slide.js
+++ public/js/banner-slide.js
@@ -13,9 +13,9 @@ let button6 = document.getElementById('button_banner_6')
 
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", function() {
-        let current = document.getElementsByClassName("active");
-        current[0].className = current[0].className.replace(" active", "");
-        this.className += " active";
+        let current = document.getElementsByClassName("activ");
+        current[0].className = current[0].className.replace(" activ", "");
+        this.className += " activ";
       });
 }
 
