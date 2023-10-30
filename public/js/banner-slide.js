ae747168c4c7f091e79d7cc7ce2ae56825ee6cbc
Mark-Kil <markkilcanarias@gmail.com>
2023-10-30 14:50:08 +0100

    errors fixed, photo do no appear

--- public/js/banner-slide.js
+++ public/js/banner-slide.js
@@ -2,8 +2,9 @@ const banner_images = document.querySelector('.banner_images');
 const button_container = document.querySelectorAll('.button_container button');
 
 button_container.forEach((button, index) => {
-    button.addEventListener('click', () => {
-        const slideWidth = document.querySelector('.slide').offsetWidth;
+    button.addEventListener('click', (event) => {
+        event.preventDefault()
+        const slideWidth = document.querySelector('.banner_images').offsetWidth;
         banner_images.style.transform = `translateX(-${index * slideWidth}px)`;
     });
 });
