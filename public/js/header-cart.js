34bdc313762c901155a619aa4c791fd6f714f259
Mark-Kil <markkilcanarias@gmail.com>
2023-11-02 08:35:02 +0100

    code cleared

--- public/js/header-cart.js
+++ public/js/header-cart.js
@@ -10,36 +10,6 @@ addToCartButton.addEventListener('click', (e) => {
 
     clicked = false
 
-    // if (clicked == false) {
-    //     cartIndex =+ 1
-    //     headerFullCart.style.display = 'none';
-    //     headerEmptyCart.style.display = 'block';
-    //     clicked = true
-    // }
-
-    // if (clicked == true) {
-    //     cartIndex =- 1
-    //     headerFullCart.style.display = 'block';
-    //     headerEmptyCart.style.display = 'none';
-    //     clicked = false
-    // }
-
-    // if (headerFullCart.style.display == 'none') {
-    //     cartIndex += 1
-    //     headerFullCart.style.display = 'block';
-    //     headerEmptyCart.style.display = 'none';
-    //     let newNumElement = document.createElement('p')
-    //     let newNumber = document.createTextNode(`${cartIndex}`)
-    //     newNumElement.appendChild(newNumber)
-    //     headerCart.insertBefore(newNumElement, headerEmptyCart)
-    // } else {
-    //     cartIndex += 1
-    //     let newestNumElement = document.createElement('p')
-    //     let newestNumber = document.createTextNode(`${cartIndex}`)
-    //     newestNumElement.appendChild(newestNumber)
-    //     newNumElement.replaceWith(newestNumElement)
-    // }
-
     if (headerEmptyCart.style.display == 'block') {
         cartIndex +=1
         headerCart.innerHTML = /* html */`
